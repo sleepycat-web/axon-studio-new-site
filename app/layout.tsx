a2c685cb742ff326f3317a270bdf24a5d591440d
Amlan Sarmah <amlansarmah26@gmail.com>
2024-06-28 14:13:04 +0530

    open graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,40 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-neutral-950">
-      <link rel="canonical"
-        href="https://axonstudio.tech/"
-        key="canonical" />
+      {/* Open Graph Meta Tags */}
+      <meta property="og:url" content="https://axonstudio.tech" />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:title"
+        content="Axon Studio | Web Development Agency in Siliguri"
+      />
+      <meta
+        property="og:description"
+        content="Welcome to Axon Studio, your go-to partner for online growth! We are a team of experts who will elevate your brand's potential one click at a time."
+      />
+      <meta
+        property="og:image"
+        content="https://axonstudio.tech/assets/hero.webp"
+      />
+
+      {/* Twitter Meta Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="axonstudio.tech" />
+      <meta property="twitter:url" content="https://axonstudio.tech" />
+      <meta
+        name="twitter:title"
+        content="Axon Studio | Web Development Agency in Siliguri"
+      />
+      <meta
+        name="twitter:description"
+        content="Welcome to Axon Studio, your go-to partner for online growth! We are a team of experts who will elevate your brand's potential one click at a time."
+      />
+      <meta
+        name="twitter:image"
+        content="https://axonstudio.tech/assets/hero.webp"
+      />
+
+      <link rel="canonical" href="https://axonstudio.tech/" key="canonical" />
       <body className={inter.className}>{children}</body>
     </html>
   );
