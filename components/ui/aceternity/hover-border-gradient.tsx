1804cde4fa4da3d1d6ab471e63454ebf5c8b3c92
Amlan Sarmah <amlansarmah26@gmail.com>
2024-06-28 13:51:20 +0530

    Hari Om

--- components/ui/aceternity/hover-border-gradient.tsx
+++ components/ui/aceternity/hover-border-gradient.tsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect, useCallback } from "react";
+"use client";
+import React, { useState, useEffect, useRef } from "react";
+
 import { motion } from "framer-motion";
 import { cn } from "@/utils/cn";
 
@@ -24,26 +26,14 @@ export function HoverBorderGradient({
   const [hovered, setHovered] = useState<boolean>(false);
   const [direction, setDirection] = useState<Direction>("TOP");
 
-  const rotateDirection = useCallback(
-    (currentDirection: Direction): Direction => {
-      const directions: Direction[] = ["TOP", "LEFT", "BOTTOM", "RIGHT"];
-      const currentIndex = directions.indexOf(currentDirection);
-      const nextIndex = clockwise
-        ? (currentIndex - 1 + directions.length) % directions.length
-        : (currentIndex + 1) % directions.length;
-      return directions[nextIndex];
-    },
-    [clockwise]
-  );
-
-  useEffect(() => {
-    if (!hovered) {
-      const interval = setInterval(() => {
-        setDirection((prevState) => rotateDirection(prevState));
-      }, duration * 1000);
-      return () => clearInterval(interval);
-    }
-  }, [hovered, duration, rotateDirection]);
+  const rotateDirection = (currentDirection: Direction): Direction => {
+    const directions: Direction[] = ["TOP", "LEFT", "BOTTOM", "RIGHT"];
+    const currentIndex = directions.indexOf(currentDirection);
+    const nextIndex = clockwise
+      ? (currentIndex - 1 + directions.length) % directions.length
+      : (currentIndex + 1) % directions.length;
+    return directions[nextIndex];
+  };
 
   const movingMap: Record<Direction, string> = {
     TOP: "radial-gradient(20.7% 50% at 50% 0%, hsl(0, 0%, 100%) 0%, rgba(255, 255, 255, 0) 100%)",
@@ -57,6 +47,14 @@ export function HoverBorderGradient({
   const highlight =
     "radial-gradient(75% 181.15942028985506% at 50% 50%, #3275F8 0%, rgba(255, 255, 255, 0) 100%)";
 
+  useEffect(() => {
+    if (!hovered) {
+      const interval = setInterval(() => {
+        setDirection((prevState) => rotateDirection(prevState));
+      }, duration * 1000);
+      return () => clearInterval(interval);
+    }
+  }, [hovered]);
   return (
     <Tag
       onMouseEnter={(event: React.MouseEvent<HTMLDivElement>) => {
@@ -64,14 +62,14 @@ export function HoverBorderGradient({
       }}
       onMouseLeave={() => setHovered(false)}
       className={cn(
-        "relative flex rounded-full border  content-center  hover:bg-black/10 transition duration-500 bg-white/20 items-center flex-col flex-nowrap gap-10 h-min justify-center overflow-visible p-px decoration-clone w-fit",
+        "relative flex rounded-full border  content-center bg-black/20 hover:bg-black/10 transition duration-500 dark:bg-white/20 items-center flex-col flex-nowrap gap-10 h-min justify-center overflow-visible p-px decoration-clone w-fit",
         containerClassName
       )}
       {...props}
     >
       <div
         className={cn(
-          "w-auto text-white z-10 bg-neutral-950 px-4 py-2 rounded-[inherit]",
+          "w-auto text-white z-10 bg-black px-4 py-2 rounded-[inherit]",
           className
         )}
       >
@@ -102,7 +100,7 @@ export function HoverBorderGradient({
 
 export function HoverBorderGradientDemo() {
   return (
-    <div className=" fixed bottom-8 right-8">
+    <div className=" fixed text-center bottom-8 right-8">
       <HoverBorderGradient
         containerClassName="rounded-full"
         as="button"
