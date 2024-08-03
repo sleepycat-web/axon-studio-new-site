e3726bac29e8c411507623ac889887426a525373
Amlan Sarmah <amlansarmah26@gmail.com>
2024-08-03 23:44:33 +0530

    admin commieter

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,10 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
   title: "Axon Studio | Web Development Agency in Siliguri",
   description: "Welcome to Axon Studio, your go-to partner for online growth! We are a team of experts who will elevate your brand's potential one click at a time.",
-};
+}
 
 export default function RootLayout({
   children,
