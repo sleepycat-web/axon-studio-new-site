52daea6b764889a9d3828027d025d41798766294
Amlan Sarmah <amlansarmah26@gmail.com>
2024-11-15 01:08:39 +0530

    flip words demo rem

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,14 +6,12 @@ import Details from "../../components/ui/contact/details";
 import ContactForm from "../../components/ui/contact/form";
 
 import Calendly from "../../components/ui/contact/calendly";
-import { FlipWordsDemo } from "@/components/ui/aceternity/flip-words";
 
 export default function Main() {
   return (
     <div className="bg-neutral-950">
       <Navbar />
 
-      {/* <FlipWordsDemo /> */}
       <Calendly />
       <Details />
       <ContactForm />
