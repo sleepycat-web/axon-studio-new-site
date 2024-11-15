1a9f57eda3c829a9ab21fc8a89cbed2e6988ca44
Amlan Sarmah <amlansarmah26@gmail.com>
2024-11-15 12:39:30 +0530

    make chages for calendly widget and confirmation message while removing form,

---
+++ components/ui/contact/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import Navbar from "@/components/ui/layout/header";
+import Footer from "@/components/ui/layout/footer";
+import Details from "@/components/ui/contact/details";
+import ContactForm from "@/components/ui/contact/form";
+
+import Calendly from "@/components/ui/contact/calendly";
+
+export default function Main() {
+  return (
+    <div className="bg-neutral-950">
+      <Navbar />
+      <Calendly />
+      <Details />
+      <ContactForm />
+      <Footer />
+    </div>
+  );
+}
